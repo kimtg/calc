be5f132e966921cf6f1a18d6cf584f712c3af158
KIM Taegyoon <steloflute@gmail.com>
2016-02-20 23:24:51 +0900

    1

---
+++ calc.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char * argv[])
+{
+	double a;
+	char buf[80];
+	puts("Operators: + - * / sqrt expt");
+	puts("Tokens are separated by whitespaces.");
+
+	while (true) {
+		printf("> ");
+		scanf_s("%s", buf);
+		if (strcmp(buf, "+") == 0) {
+			double b;
+			scanf_s("%lf", &b);
+			a += b;
+		}
+		else if (strcmp(buf, "-") == 0) {
+			double b;
+			scanf_s("%lf", &b);
+			a -= b;
+		}
+		else if (strcmp(buf, "*") == 0) {
+			double b;
+			scanf_s("%lf", &b);
+			a *= b;
+		}
+		else if (strcmp(buf, "/") == 0) {
+			double b;
+			scanf_s("%lf", &b);
+			a /= b;
+		}
+		else if (strcmp(buf, "sqrt") == 0) {
+			a = sqrt(a);
+		}
+		else if (strcmp(buf, "expt") == 0) {
+			double b;
+			scanf_s("%lf", &b);
+			a = pow(a, b);
+		}
+		else {
+			a = atof(buf);
+		}
+		printf("%.16g\n", a);
+	}
+
+	return 0;
+}
